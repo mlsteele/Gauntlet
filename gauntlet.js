9d2dd0c552acda78eef4dd67be9eaa0c821eca4d
Miles Steele <miles@milessteele.com>
2012-02-18 22:02:55 -0500

    refactor

--- gauntlet.js
+++ gauntlet.js
@@ -10,6 +10,7 @@ var gameserver = net.Server(function(socket) {
     console.log('socket ['+socketID+'] < "'+msg.replace(/(\r\n|\n|\r)/gm,'\\n')+'"');
     return socket.write(msg);
   });}
+  socketWriteAll.alive = true;
   
   openSockets.push(socket);
   socket.setEncoding('utf8');
@@ -18,21 +19,22 @@ var gameserver = net.Server(function(socket) {
   
   socket.write('\nHello. You are on socket ['+socketID+']\n');
   
-  var prompt = gauntlet('', socketWriteAll);
+  var prompt = gauntlet(socketWriteAll);
   
   socket.on('data', function(data){
     var fdata = data.substring(0, data.length-2);
     console.log('socket ['+socketID+'] > "'+fdata+'"');
     
-    prompt = prompt(fdata, socketWriteAll);
+    prompt = prompt(fdata);
     if (!prompt) socket.end();
   });
   
   socket.on('close', function(had_error) {
     openSockets = _U.without(openSockets, socket);
+    socketWriteAll.alive = false;
     console.log('-- socket ['+socketID+'] closed' + (had_error ? ' with error!' : ''));
   })
-}).listen(8000);
+}).listen(8007);
 
 
 var controlserver = net.Server(function(socket) {
@@ -102,10 +104,10 @@ function gcontrol(talkback) {
   }
 }
 
-function gauntlet(response, talkback) {
-  talkback('\nWill you enter the guantlet?\n\n-- ');
+function gauntlet(talkback) {
+  talkback('\nWill you enter the gauntlet?\n\n-- ');
   
-  return function gateway(response, talkback) {
+  return function gateway(response) {
     if (matchAction(response, ['enter', gwords.yes, 'go', 'run'])) {
       var hallmsg = 'You are running through a grey stone hallway. '+
                     'Sharp pieces of metal poke out at strange angles '+
@@ -114,7 +116,7 @@ function gauntlet(response, talkback) {
                     'and hissing sounds but you are too terrified to look.\n';
       talkback('\n'+hallmsg+'\n-- ');
       
-      var loopback = function(response, talkback) {
+      var loopback = function(response) {
         if (callback === loopback) {
           if (matchAction(response, ['help', 'what?'])) {
             talkback('\nThere is no help in sight.\n');
@@ -122,46 +124,133 @@ function gauntlet(response, talkback) {
           } else if (matchAction(response, ['look', 'hall', 'hallway'])) {
             talkback('\n'+hallmsg+'\n');
             talkback('Just keep running.\n\n-- ');
+          } else if (matchAction(response, gwords.jump)) {
+            talkback('\nYou look like a fool jumping up and down.\n\n-- ');
           } else {
-            talkback('\nThat isn\'t really relevant given your predicament.\n');
+            if (response.length > 0)
+              talkback('\nThat isn\'t really relevant given your predicament.');
             talkback('\nJust keep running.\n\n-- ');
           }
           return callback;
         } else {
-          return callback(response, talkback);
+          return callback(response);
         }
       };
       var callback = loopback;
       
-      setTimeout(function(){
-        // WARN: talkback may be stale.
-        talkback('\n\nA wall approaches from ahead.\nQuickly, jump!\n\n-- ');
-        
-        var consequence = '\n\n\You ran kersplat into a wall. '+
-                          'The wall is red and you are dead.\n\n';
-        var timeout = function(){
-          talkback(consequence);
-          callback = function(){return false};
-        };
-        setTimeout(function(){return timeout()}, 3000);
-        
-        callback = function(response, talkback) {
-          if (matchAction(response, gwords.jump)) {
-            talkback('\nThe wall whooses beneath your feet. You land '+
-                     'and continue running.\nWell done.\n\n-- ');
-            timeout = function(){};
-            callback = loopback;
-            return callback;
-          } else {
+      var obstacles = {
+            wall: {
+              name: 'wall',
+              warn: 'A stone wall approaches from ahead.\nQuickly, jump!\n',
+              pass: 'The wall whooses beneath your feet. You land and continue running.\n',
+              fail: 'You ran kersplat into a wall. The wall is red and you are dead.\n',
+              predicate: function(response) {
+                if (matchAction(response, gwords.jump))
+                  return {pass: true, msg: this.pass}
+                else
+                  return {pass: false, msg: this.fail}
+              }
+            },
+            hole: {
+              name: 'hole',
+              warn: 'Is that a hole in the ground?\n',
+              pass: 'You look down as you bound over the hole.\nThere are bodies.\nCreepy.\n',
+              fail: 'No, that\'s just a black spot on the gr-\nAaaaaaaah...\n',
+              yeah: 'Yes it is. You\'re dead.\n',
+              predicate: function(response) {
+                if (matchAction(response, gwords.jump))
+                  return {pass: true, msg: this.pass}
+                else if (matchAction(response, gwords.no))
+                  return {pass: true, msg: this.yeah}
+                else
+                  return {pass: false, msg: this.fail}
+              }
+            },
+            beam: {
+              name: 'beam',
+              warn: 'A beam stretches across in front of you at head level.\n',
+              pass: 'You slip under the beam just in time.\n',
+              fail: ['Smack!\nYou dropped something.\n',
+                    'You try to jump above head level but that\'s impossible. '+
+                    'You fall on the beam and become dog food.\n'],
+              predicate: function(response) {
+                if (matchAction(response, gwords.under))
+                  return {pass: true, msg: this.pass}
+                else if (matchAction(response, gwords.jump))
+                  return {pass: false, msg: this.fail[1]}
+                else
+                  return {pass: false, msg: this.fail[0]}
+              }
+            },
+            bat: {
+              name: 'bat',
+              warn: 'A horrifying vampirous bat is flitting around your head.\n',
+              pass: 'You pummel the thing until it hisses green smoke and disintegrates.\n',
+              fail: 'The bat leaches onto your ankle which turns purple, then yellow, '+
+                    'and then explodes. You are eaten.\n',
+              flail: ['You clutch at the air randomly committing violence at everything but '+
+                     'the offending bat. Specificity is the word of the day.\n',
+                     'Attack what, the wall?\nSpecificity is the word of the day.\n'],
+              jump: 'Jump... over a bat?\n'+
+                    'The bat leaches onto your ankle which turns purple, then yellow, '+
+                    'and then explodes. You are eaten.\n',
+              predicate: function(response) {
+                var batnames = ['him', 'her', 'it', 'bat', 'creature', 'vampire'];
+                if (matchAction(response, gwords.attack, batnames))
+                  return {pass: true, msg: this.pass}
+                else if (matchAction(response, gwords.attack))
+                  return {pass: false, msg: this.flail[Math.floor(Math.random()*2)]}
+                else if (matchAction(response, gwords.jump))
+                  return {pass: false, msg: this.jump}
+                else
+                  return {pass: false, msg: this.fail}
+              }
+            }
+          };
+      var randObstacle = function() {
+        var l = _U.values(obstacles);
+        return l[Math.floor(Math.random() * l.length)];
+      }
+      
+      var placeObstacle = function(obstacle, timeuntil, responsewindow, cb) {
+        setTimeout(function(){
+          if (!talkback.alive) return;
+          
+          talkback('\n\n'+obstacle.warn+'\n-- ');
+          
+          var consequence = '\n\n'+obstacle.predicate('').msg+'\n';
+          var timeout = function(){
             talkback(consequence);
-            return false;
-          }
-        };
-      }, 5000);
+            talkback.alive = false;
+            callback = function(){return (callback = false)};
+          };
+          setTimeout(function(){return talkback.alive ? timeout() : undefined}, responsewindow);
+          
+          callback = function(response) {
+            var state = obstacle.predicate(response);
+            if (state.pass) {
+              talkback('\n'+state.msg+'\n-- ');
+              timeout = function(){};
+              return (callback = loopback);
+            } else {
+              talkback('\n'+state.msg+'\n');
+              talkback.alive = false;
+              return (callback = function(){return false});
+            }
+          };
+          
+          cb && cb();
+        }, timeuntil);
+      }
+      
+      setTimeout(function placeLoop(){
+        if (talkback.alive)
+          placeObstacle(randObstacle(), Math.random() * 5000 + 2500, 7000, placeLoop);
+      }, 4000);
       
       return callback;
     } else {
-      talkback('\nOk. Goodbye.\n\n');
+      talkback('\nHa. Goodbye.\n\n');
       return false;
     }
   }
@@ -169,8 +258,10 @@ function gauntlet(response, talkback) {
 
 var gwords = {
   jump: ['jump', 'hop', 'skip', 'leap', 'bound', 'vault', 'sail'],
-  yes: ['yes', 'yeah', 'y', 'ye', 'ues'],
-  attack: ['hit', 'smack', 'kill', 'mame', 'destroy', 'punch', 'kick', 'chop', 'attack']
+  yes: ['yes', 'yeah', 'y', 'ye', 'ues', 'sure'],
+  no: ['no', 'negative', 'not'],
+  attack: ['hit', 'smack', 'kill', 'mame', 'destroy', 'punch', 'kick', 'chop', 'attack'],
+  under: ['slide', 'roll', 'duck', 'under', 'crouch', 'dive']
 }
 
 var matchAction = function(response) {
